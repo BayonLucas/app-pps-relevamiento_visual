f53c970ffc26e7b62f87229e09928c8422acb58e
BayonLucas <lucas.bayon31@hotmail.com>
2024-06-08 11:55:10 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app-relevamiento-visual',
+  appId: 'com.relevamiento_visual.app',
+  appName: 'app',
   webDir: 'www'
 };
 
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +19,10 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({"projectId":"ppappspps","appId":"1:906736048341:web:39a53f94075956c33eea5c","storageBucket":"ppappspps.appspot.com","apiKey":"AIzaSyCBB8TkVPVYEphO_KYIJht2URLOSLx8crI","authDomain":"ppappspps.firebaseapp.com","messagingSenderId":"906736048341"})), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore()), 
+    provideStorage(() => getStorage()),
   ],
 });
